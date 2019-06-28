0b62c6cfbec7bb4496d854d5a1a8075a289cacc0
ingRamiroContreras <ramirocontreras65@gmail.com>
2019-06-27 22:58:17 -0500

    update model

--- masivian/src/main/java/com/masivian/app/modelo/Arbol.java
+++ masivian/src/main/java/com/masivian/app/modelo/Arbol.java
@@ -20,62 +20,54 @@ public class Arbol {
 		this.raiz = raiz;
 	}
 
-	private void addNodo( Nodo nodo, Nodo raiz ) {
-        if ( raiz == null ) {
-            this.setRaiz(nodo);
-        }
-        else {
-            if ( nodo.getValor() <= raiz.getValor() ) {
-                if (raiz.getHojaIzquierda() == null) {
-                    raiz.setHojaIzquierda(nodo);
-                }
-                else {
-                    addNodo( nodo , raiz.getHojaIzquierda() );
-                }
-            }
-            else {
-
-                if (raiz.getHojaDerecha() == null) {
-                    raiz.setHojaDerecha(nodo);
-                }
-                else {
-                    addNodo( nodo, raiz.getHojaDerecha() );
-                }
-            }
-        }
-    }
-
 	public void addNodo(Nodo nodo) {
 		this.addNodo(nodo, this.raiz);
 	}
-	
-	public Nodo findLCA(int n1, int n2) 
-    { 
-        return findLCA(this.raiz, n1, n2); 
-    }
-	
-	private Nodo findLCA(Nodo node, int n1, int n2) 
-    { 
-        // Base case 
-        if (node == null) 
-            return null; 
-  
-        
-        if (node.getValor() == n1 || node.getValor() == n2) 
-            return node; 
-  
-        Nodo left_lca = findLCA(node.getHojaIzquierda(), n1, n2);
-        System.out.println("*********** nodo left *********");
-        System.out.println(left_lca!=null ? left_lca.getValor() : "null left");
-        Nodo right_lca = findLCA(node.getHojaDerecha(), n1, n2); 
-        System.out.println("*********** nodo right *********");
-        System.out.println(right_lca!=null ? right_lca.getValor() : "null right");
-  
-        if (left_lca!=null && right_lca!=null) 
-            return node; 
-  
-        return (left_lca != null) ? left_lca : right_lca; 
-    } 
-  
+
+	private void addNodo(Nodo nodo, Nodo raiz) {
+		if (raiz == null) {
+			this.setRaiz(nodo);
+		} else {
+			if (nodo.getValor() <= raiz.getValor()) {
+				if (raiz.getHojaIzquierda() == null) {
+					raiz.setHojaIzquierda(nodo);
+				} else {
+					addNodo(nodo, raiz.getHojaIzquierda());
+				}
+			} else {
+
+				if (raiz.getHojaDerecha() == null) {
+					raiz.setHojaDerecha(nodo);
+				} else {
+					addNodo(nodo, raiz.getHojaDerecha());
+				}
+			}
+		}
+	}
+
+	public Nodo findLCA(int n1, int n2) {
+		return findLCA(this.raiz, n1, n2);
+	}
+
+	private Nodo findLCA(Nodo node, int n1, int n2) {
+		// Base case
+		if (node == null)
+			return null;
+
+		if (node.getValor() == n1 || node.getValor() == n2)
+			return node;
+
+		Nodo left_lca = findLCA(node.getHojaIzquierda(), n1, n2);
+		System.out.println("*********** nodo left *********");
+		System.out.println(left_lca != null ? left_lca.getValor() : "null left");
+		Nodo right_lca = findLCA(node.getHojaDerecha(), n1, n2);
+		System.out.println("*********** nodo right *********");
+		System.out.println(right_lca != null ? right_lca.getValor() : "null right");
+
+		if (left_lca != null && right_lca != null)
+			return node;
+
+		return (left_lca != null) ? left_lca : right_lca;
+	}
 
 }
